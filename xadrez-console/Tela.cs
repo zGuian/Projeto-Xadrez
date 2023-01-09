9fa18b5bb4c3a7c0e8db5e4b7f7edd07f090bfc5
Guian Marcos <Guianmarcos32@gmail.com>
2023-01-09 13:32:13 -0300

    Melhorando a implementação do metodo imprimirTabuleiro

--- xadrez-console/Tela.cs
+++ xadrez-console/Tela.cs
@@ -9,6 +9,7 @@ namespace xadrez_console
         {
             for (int i = 0; i < tab.linha; i++)
             {
+                Console.Write(8 - i + " ");
                 for (int j = 0; j < tab.coluna; j++)
                 {
                     if (tab.peca(i, j) == null)
@@ -17,10 +18,26 @@ namespace xadrez_console
                     }
                     else
                     {
-                        Console.Write(tab.peca(i, j) + " ");
+                        Tela.imprimirPeca(tab.peca(i, j));
                     }
                 }
                 Console.WriteLine();
+            }
+            Console.WriteLine("  a b c d e f g h");
+        }
+
+        public static void imprimirPeca(Peca peca)
+        {
+            if(peca.cor == Cor.Branca)
+            {
+                Console.Write(peca);
+            }
+            else
+            {
+                ConsoleColor aux = Console.ForegroundColor;
+                Console.ForegroundColor = ConsoleColor.Yellow;
+                Console.Write(peca);
+                Console.ForegroundColor = aux;
             }
         }
     }
